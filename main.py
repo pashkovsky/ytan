8edc03f6e0ff2a0f11dd88cf1a8a79b6c6d14130
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2018-07-08 11:50:03 +0300

    Add class SearchVideo. There is a bug with mix of q&key's values

--- main.py
+++ main.py
@@ -13,3 +13,26 @@ class Client:
         return response
 
 
+class SearchVideo:
+
+    def __init__(self, keyword, api_key):
+        self.keyword = keyword
+        self.api_key = api_key
+        self.url_add = "search"
+
+    def search(self):
+        client = Client(self.api_key)
+        client.url = client.url + self.url_add
+        print("client.url: ", client.url)                   # delete
+        params = {
+            "maxResults" : 25,
+            "part" : "snippet",
+            "key" : self.api_key,
+            "q" : self.keyword}
+
+        print("params: ", params)                           # delete
+
+        data = client._get(params=params)
+        print("data: ", data)                               # delete
+        return data
+
