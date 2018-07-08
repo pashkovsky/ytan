bfcc18481aaa2bde629c866a7035cd9ac7253b5f
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2018-07-08 09:29:05 +0300

    Fix error Response 400 (by **params). Now module send response 200

--- http_client.py
+++ http_client.py
@@ -2,13 +2,11 @@ import requests
 
 
 class Client:
-
-    url = "https://www.googleapis.com/youtube/v3"
     
     def __init__(self, api_key):
         self.api_key = api_key
 
 
-    def get(self, url, **params):
+    def get(self, url, params):
         response = requests.get(url=url, params=params)
         return response
